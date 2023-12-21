9e714be5f8ad8ab7faed6a4b8a5c482717b127cd
Steve LeValley <slevalley@gmail.com>
2023-12-21 10:55:39 -0800

    Added cis benchmarks

---
+++ cis-master.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#cis-master.sh
+
+# ./kube-bench --config-dir `pwd`/cfg --config `pwd`/cfg/config.yaml master --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail
+
+# total_fail=$(kube-bench master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .[].total_fail)
+
+total_fail=$(./kube-bench --config-dir `pwd`/cfg --config `pwd`/cfg/config.yaml master --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
+
+if [[ "$total_fail" -ne 0 ]];
+        then
+                echo "CIS Benchmark Failed MASTER while testing for 1.2.7, 1.2.8, 1.2.9"
+                exit 1;
+        else
+                echo "CIS Benchmark Passed for MASTER - 1.2.7, 1.2.8, 1.2.9"
+fi;
